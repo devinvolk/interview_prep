7af63519346d5cd6fed4b52091e4ff52c07a1f60
devinvolk@comcast.net <devinvolk@Devins-MacBook-Air.local>
2023-10-26 11:43:41 -0700

    stack question added

--- stack.py
+++ stack.py
@@ -41,3 +41,42 @@ def daily_temp(temperatures):
     return res
 
 print(daily_temp([73,74,75,71,69,72,76,73]))
+
+# 150. Evaluate Reverse Polish Notation
+# You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+# Evaluate the expression. Return an integer that represents the value of the expression.
+
+# Note that:
+
+# The valid operators are '+', '-', '*', and '/'.
+# Each operand may be an integer or another expression.
+# The division between two integers always truncates toward zero.
+# There will not be any division by zero.
+# The input represents a valid arithmetic expression in a reverse polish notation.
+# The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+def rev_polish(tokens):
+    #Time Complexity: O(n)
+    #Space Complexity: O(n)
+
+    stack = []
+
+    for i in tokens:
+        if i == '+':
+            stack.append(stack.pop() + stack.pop())
+        elif i == '-':
+            x = stack.pop()
+            y = stack.pop()
+            stack.append(y-x)
+        elif i == '*':
+            stack.append(stack.pop() * stack.pop())
+        elif i == '/':
+            x = stack.pop()
+            y = stack.pop()
+            stack.append(int(y/x))
+        else:
+            stack.append(int(i))
+    return stack[0]
+
+print(rev_polish(["4","13","5","/","+"]))
