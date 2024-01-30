9a07023b82abac6afa4174393e419ec39b1bf9cd
devinvolk@comcast.net <devinvolk@comcast.net>
2024-01-30 13:03:07 -0800

    efficient janitor problem added

--- two_pointers.py
+++ two_pointers.py
@@ -35,3 +35,28 @@ def valid_palindrome2(s):
     return test == test[::-1]
 
 print(valid_palindrome2("A man, a plan, a canal: Panama"))
+
+# Find the minimum number of groups who's sum of each group is at max 3, and every element must be in a group.
+# Given an Array like: [1.01, 1.01, 3.0, 2.7, 1.99, 2.3, 1.7]
+# return the minimum number of groups, in this case it would be 5 groups: (1.01 , 1.99), (1.01, 1.7), (3.0), (2.7), (2.3)
+# Constraint: all elements are between 1.01-3 inclucsive, and each groups sum is at max 3
+
+def efficient_janitor(lst):
+    lst.sort(reverse=True)
+    l, r = 0, len(lst)-1
+    used_idx = set()
+    groups = 0
+
+    while l <= r:
+        sum = lst[l]
+        used_idx.add(l)
+        while l < r and sum + lst[r] <= 3 and r not in used_idx:
+            sum += lst[r]
+            used_idx.add(r)
+            r -= 1
+        l += 1
+        groups += 1
+    
+    return groups
+
+print(efficient_janitor([1.01, 1.01, 3.0, 2.7, 1.99, 2.3, 1.7]))
